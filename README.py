335cc87422be809f3a69d0c730be8d00e96756b5
Omar Sadigli <62852026+OmarSadigli@users.noreply.github.com>
2021-01-27 04:16:50 -0800

    Update README.py

--- README.py
+++ README.py
@@ -1,4 +1,4 @@
 # Higher-or-Lower-game
 '''
-This project createn on repl.it. So in main.py I have imported clear() function from replit library. You can use this function on repl.it.
+This project created on repl.it. So in main.py I have imported clear() function from replit library. You can use this function on repl.it.
 '''
